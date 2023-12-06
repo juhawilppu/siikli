aab658714fa162b8af1592abccce74ae355fbe02
Juha Wilppu <juha.wilppu@nordhealth.com>
2023-12-06 16:50:13 +0200

    Add validation and types to endpoint

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { z } from 'zod'
 import { rateLimit } from '../../middlewares/rateLimit'
+import { validate } from '../../middlewares/validate'
 
 export const postsRoute = express.Router()
 const prisma = new PrismaClient()
@@ -10,13 +12,34 @@ postsRoute.get(`/api/posts`, async (req, res) => {
   res.json(result)
 })
 
-postsRoute.post(`/api/posts`, rateLimit(5, 5), async (req, res) => {
-  const { title, content } = req.body
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-    },
-  })
-  res.json(result)
+const PostPostsSchema = z.object({
+  body: z.object({
+    title: z
+      .string({
+        required_error: 'Title is required',
+      })
+      .max(128, { message: 'Title has max length 128' }),
+    content: z
+      .string({
+        required_error: 'Content is required',
+      })
+      .max(512, { message: 'Content has max length 512' }),
+  }),
 })
+type PostPostsRequestDto = z.infer<typeof PostPostsSchema>
+interface PostPostsRequest extends Omit<Request, 'body'>, PostPostsRequestDto {}
+postsRoute.post(
+  `/api/posts`,
+  rateLimit(5, 5),
+  validate(PostPostsSchema),
+  async (req: PostPostsRequest, res: Response) => {
+    const { title, content } = req.body
+    const result = await prisma.post.create({
+      data: {
+        title,
+        content,
+      },
+    })
+    res.json(result)
+  }
+)
