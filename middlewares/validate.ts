f7c10c31757e505072bd34b5fa044475f090c2d5
Juha Wilppu <juha.wilppu@nordhealth.com>
2024-01-19 16:28:03 +0200

    Remove strict validation for now

--- middlewares/validate.ts
+++ middlewares/validate.ts
@@ -5,7 +5,7 @@ export const validate =
   (schema: AnyZodObject) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await schema.strict().parseAsync({
+      await schema.parseAsync({
         body: req.body,
         query: req.query,
         params: req.params,
