428b536215d67b3e2fb76396a6ea6cf8350e6a2d
Juha Wilppu <juha.wilppu@nordhealth.com>
2024-01-23 18:13:11 +0200

    Add active/inactive styles to drawer

--- frontend/src/SiikliDrawer.tsx
+++ frontend/src/SiikliDrawer.tsx
@@ -13,7 +13,7 @@ import InvoicesIcon from '@mui/icons-material/Email'
 import ProductsIcon from '@mui/icons-material/Layers'
 import OwnCompanyIcon from '@mui/icons-material/Store'
 import OrdersIcon from '@mui/icons-material/Web'
-import { StyledLink } from './StyledLink'
+import { NavLink } from 'react-router-dom'
 
 const SiikliDrawer = () => {
   return (
@@ -25,78 +25,110 @@ const SiikliDrawer = () => {
     >
       <Divider />
       <List sx={{ marginTop: '60px' }}>
-        <StyledLink to='/orders' id='drawer-orders'>
+        <NavLink
+          to='/orders'
+          id='drawer-orders'
+          end
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <OrdersIcon />
             </ListItemIcon>
             <ListItemText primary='Tilaukset' />
           </ListItem>
-        </StyledLink>
-        <StyledLink to='/orders/0/edit' id='drawer-new-order'>
+        </NavLink>
+        <NavLink
+          to='/orders/0/edit'
+          id='drawer-new-order'
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <NewOrderIcon />
             </ListItemIcon>
             <ListItemText primary='Uusi tilaus' />
           </ListItem>
-        </StyledLink>
-        <StyledLink to='/packaging_list'>
+        </NavLink>
+        <NavLink
+          to='/packaging_list'
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <PackageListIcon />
             </ListItemIcon>
             <ListItemText primary='Pakkauslista' />
           </ListItem>
-        </StyledLink>
-        <StyledLink to='/invoices'>
+        </NavLink>
+        <NavLink
+          to='/invoices'
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <InvoicesIcon />
             </ListItemIcon>
             <ListItemText primary='Laskutus' />
           </ListItem>
-        </StyledLink>
-        <StyledLink to='/sales_report'>
+        </NavLink>
+        <NavLink
+          to='/sales_report'
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <SalesReportIcon />
             </ListItemIcon>
             <ListItemText primary='Myyntiraportti' />
           </ListItem>
-        </StyledLink>
-        <StyledLink to='/products' id='drawer-products'>
+        </NavLink>
+        <NavLink
+          to='/products'
+          id='drawer-products'
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <ProductsIcon />
             </ListItemIcon>
             <ListItemText primary='Tuotteet' />
           </ListItem>
-        </StyledLink>
-        <StyledLink to='/customers' id='drawer-customers'>
+        </NavLink>
+        <NavLink
+          to='/customers'
+          id='drawer-customers'
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <CustomersIcon />
             </ListItemIcon>
             <ListItemText primary='Asiakkaat' />
           </ListItem>
-        </StyledLink>
-        <StyledLink to='/package_configuration'>
+        </NavLink>
+        <NavLink
+          to='/package_configuration'
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <OwnCompanyIcon />
             </ListItemIcon>
             <ListItemText primary='Pakkausasetukset' />
           </ListItem>
-        </StyledLink>
-        <StyledLink to='/own_company'>
+        </NavLink>
+        <NavLink
+          to='/own_company'
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
           <ListItem button>
             <ListItemIcon>
               <OwnCompanyIcon />
             </ListItemIcon>
             <ListItemText primary='Oma yritys' />
           </ListItem>
-        </StyledLink>
+        </NavLink>
       </List>
     </Drawer>
   )
