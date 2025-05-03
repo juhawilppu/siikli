43a568789d6b3882411621ef26026d9f82f334d8
Juha Wilppu <juha.wilppu@gmail.com>
2025-05-03 21:26:27 +0300

    S-60 Use personal email for now

--- frontend/src/Support.tsx
+++ frontend/src/Support.tsx
@@ -27,10 +27,10 @@ export default function ContactPage() {
                             <h2 className="text-3xl font-bold tracking-tight">Tuki</h2>
                             <div className="space-y-4 text-muted-foreground">
                                 <p>
-                                    <a href="mailto:tuki@siikli.fi">tuki@siikli.fi</a>
+                                    Ongelmatilanteissa ota yhteyttä:
                                 </p>
                                 <p>
-                                    050 xxx xx xx
+                                    <a className="text-blue-500" href="mailto:juha.wilppu@gmail.com">juha.wilppu@gmail.com</a>
                                 </p>
                             </div>
                         </div>
