b7be887dac6ca3d0587fbe4371e6ec050414fc3d
Juha Wilppu <juha.wilppu@gmail.com>
2025-06-19 05:19:41 +0300

    S-266 Remove unused helper files

--- init_localhost.sh
+++ init_localhost.sh
@@ -1,6 +1,8 @@
 npx prisma format
 npx prisma migrate reset -f
+# npx prisma migrate deploy
+
 npm i -D tsx
-#npx prisma migrate dev --name rename_company_settings_columns
+# npx prisma migrate dev --name rename_company_settings_columns
 
 npx tsx ./src/dev/verify-history-tables.ts
