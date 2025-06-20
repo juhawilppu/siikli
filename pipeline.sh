965344084c8a5133de1ef17dcc13815875e7b803
Juha Wilppu <juha.wilppu@gmail.com>
2025-06-20 12:19:07 +0300

    S-274 Make pipeline nicer and improve error handling

--- pipeline.sh
+++ pipeline.sh
@@ -1,25 +1,55 @@
+#!/bin/bash
 set -e
+start=$(date +%s)
 
+command -v docker >/dev/null 2>&1 || { echo >&2 "❌ Docker is not installed. Aborting."; exit 1; }
+command -v aws >/dev/null 2>&1 || { echo >&2 "❌ AWS CLI is not installed. Aborting."; exit 1; }
+
+echo "🔍 Running tests..."
+npm run test
+
+echo "🔍 Running linter..."
 npm run lint
-npm run format
 
-cd frontend
-npm run build
-cp not_ready.html ./dist/index.html
-aws s3 sync ./dist/ s3://siikli.fi --delete
-cd ..
+echo "🔍 Formatting prisma schema..."
+npx prisma format
 
-export version=$(date +%s)
-echo $version > terraform/version.txt
-docker build --platform linux/amd64 -t siikli-backend:$version .
-# docker run --platform linux/arm64 -e DATABASE_URL="postgresql://siikli:testpassword@host.docker.internal:5432/siikli" -p 3033:3033 siikli-backend:$version
-docker tag siikli-backend:$version 337909750746.dkr.ecr.eu-north-1.amazonaws.com/siikli-backend:$version
+# Application version
+version=$(date +%s)
 
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 337909750746.dkr.ecr.eu-north-1.amazonaws.com
+echo "🏗️ Building frontend..."
+(
+    cd frontend
+    npm run build
+    cp not_ready.html ./dist/index.html
+)
 
-docker push 337909750746.dkr.ecr.eu-north-1.amazonaws.com/siikli-backend:$version
+echo "🏗️ Building backend..."
+(
+    ECR_REPO=337909750746.dkr.ecr.eu-north-1.amazonaws.com/siikli-backend
+    echo $version > terraform/version.txt
+    docker build --platform linux/amd64 -t siikli-backend:$version .
+    docker tag siikli-backend:$version $ECR_REPO:$version
+    aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin $ECR_REPO
+    docker push $ECR_REPO:$version
+    echo "Pushed siikli-backend:$version to ECR"
+)
 
-echo "Pushed siikli-backend:$version to ECR"
+echo "🚀 Deploying frontend..."
+(
+    cd frontend
+    aws s3 sync ./dist/ s3://siikli.fi --delete
+)
 
-cd terraform
-./deploy_to_prod.sh
+echo "🚀 Deploying backend..."
+(
+    cd terraform
+    ./deploy_to_prod.sh
+)
+
+end=$(date +%s)
+echo "--------------------------------"
+echo "🎉 Deployment complete!"
+echo "📦 Application version: $version"
+echo "🌐 Environment: https://siikli.fi"
+echo "🕒 Total time: $((end - start))s"
