d74094c9f2a4b34fb0309703522a6a9aa0cd060a
Juha Wilppu <juha.wilppu@gmail.com>
2025-06-26 21:11:17 +0300

    S-284 Add tests for verify package size and type

--- src/services/tenant-service.test.ts
+++ src/services/tenant-service.test.ts
@@ -41,6 +41,19 @@ describe('tenantService', () => {
     expect(packageTypes).toBeDefined()
     expect(packageTypes.length).toBe(1)
     expect(packageTypes[0].name).toBe('Box')
-    expect(packageTypes[0].tenantId).toBe(tenant.id)
+  })
+  it('can verify package size and type', async () => {
+    const tenant = await TenantFactory.createTenant({ name: 'Test Tenant' })
+    await TenantService.verifyPackageSizeAndType('Box', 10, tenant.id)
+
+    const packageTypes = await TenantService.getPackageTypes(tenant.id)
+    expect(packageTypes).toBeDefined()
+    expect(packageTypes.length).toBe(1)
+    expect(packageTypes[0].name).toBe('Box')
+
+    const packageSizes = await TenantService.getPackageSizes(tenant.id)
+    expect(packageSizes).toBeDefined()
+    expect(packageSizes.length).toBe(1)
+    expect(packageSizes[0].size).toBe(10)
   })
 })
