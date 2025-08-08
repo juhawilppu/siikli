b378fe425a9cb063ea8a7f2fd960be5bed5226ba
Juha Wilppu <juha.wilppu@gmail.com>
2025-08-08 19:05:53 +0300

    S-306 New navbar and page style

--- frontend/src/app/components/SiikliPage.tsx
+++ frontend/src/app/components/SiikliPage.tsx
@@ -3,8 +3,8 @@ export default function SiikliPage({ children, title, description, mainAction }:
     <div className="container mx-auto p-4 md:p-6">
       <div className="mb-6 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
         <div>
-          <h1 className="text-2xl font-bold tracking-tight">{title}</h1>
-          <p className="text-muted-foreground">{description}</p>
+          <h1 className="text-2xl font-bold tracking-tight text-gray-900">{title}</h1>
+          <p className="text-gray-700">{description}</p>
         </div>
         {mainAction}
       </div>
