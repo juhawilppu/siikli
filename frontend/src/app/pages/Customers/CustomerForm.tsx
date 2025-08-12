30b7741e777906b5103a4587fd3d7384de64167a
Juha Wilppu <juha.wilppu@gmail.com>
2025-08-12 22:17:06 +0300

    S-337 Fix bug where customer modal overflows on mobile

--- frontend/src/app/pages/Customers/CustomerForm.tsx
+++ frontend/src/app/pages/Customers/CustomerForm.tsx
@@ -195,8 +195,8 @@ export function NewCustomer({ closeDialog, customerToEdit, forwaredCustomerGroup
 
   return (
     <Dialog open={true} onOpenChange={closeDialog}>
-      <DialogContent className="sm:max-w-[600px]">
-        <DialogHeader>
+      <DialogContent className="sm:max-w-[600px] max-h-[100vh] flex flex-col">
+        <DialogHeader className="flex-shrink-0">
           <DialogTitle className="text-muted-foreground">{customerToEdit ? 'Muokkaa asiakasta' : 'Uusi asiakas'}</DialogTitle>
           <DialogDescription className="text-gray-700">
             {customerToEdit ? 'Muokkaa asiakkaan tietoja.' : 'Lisää uusi asiakas.'}
@@ -204,225 +204,224 @@ export function NewCustomer({ closeDialog, customerToEdit, forwaredCustomerGroup
             Pakolliset kentät on merkitty tähdellä (*).
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4 max-h-[70vh] overflow-y-auto pl-[1px] pr-2">
-          {/* pl-[1px] to fix scrollbar width */}
-          <h2 className="text-lg font-medium">Perustiedot</h2>
-          <div className="space-y-2">
-            <Label htmlFor="edit-name" className="font-medium">
-              Nimi *
-            </Label>
-            <Input
-              id="edit-name"
-              value={customer.name}
-              onChange={e => setCustomer({ ...customer, name: e.target.value })}
-              maxLength={50}
-            />
-          </div>
-          <div className="grid grid-cols-2 gap-4">
+        <div className="flex-1 overflow-y-auto px-1">
+          <div className="grid gap-4 py-4">
+            {/* pl-[1px] to fix scrollbar width */}
+            <h2 className="text-lg font-medium">Perustiedot</h2>
             <div className="space-y-2">
-              <Label htmlFor="edit-customer_group" className="font-medium">
-                Asiakasryhmä
+              <Label htmlFor="edit-name" className="font-medium">
+                Nimi *
               </Label>
-              <Popover open={isCustomerGroupPopoverOpen} onOpenChange={setIsCustomerGroupPopoverOpen}>
-                <PopoverTrigger asChild>
-                  <Button variant="outline" role="combobox" className={`w-full justify-between ${customer.customerGroup ? '' : 'placeholder'}`}>
-                    {customer.customerGroup || 'Valitse asiakasryhmä'}
-                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-full p-0">
-                  <div className="p-2">
-                    <Input
-                      placeholder="Hae tai lisää"
-                      value={inputValueCustomerGroup}
-                      onChange={(e) => {
-                        setInputValueCustomerGroup(e.target.value)
-                      }}
-                      className="mb-2"
-                    />
-                    {inputValueCustomerGroup && !customerGroups.includes(inputValueCustomerGroup) && (
-                      <Button
-                        variant="ghost"
-                        className="w-full justify-start"
-                        onClick={handleCreateCustomerGroup}
-                      >
-                        <Plus className="w-4 h-4 mr-2" />
-                        Luo:
-                        {' '}
-                        {inputValueCustomerGroup}
-                      </Button>
-                    )}
-                  </div>
-                  <div className="max-h-[200px] overflow-y-auto">
-                    {customerGroups.sort((a, b) => a.localeCompare(b)).map(customerGroup => (
-                      <Button
-                        key={customerGroup}
-                        variant="ghost"
-                        className="w-full justify-start"
-                        onClick={() => {
-                          setCustomer({ ...customer, customerGroup })
-                          setIsCustomerGroupPopoverOpen(false)
+              <Input
+                id="edit-name"
+                value={customer.name}
+                onChange={e => setCustomer({ ...customer, name: e.target.value })}
+                maxLength={50}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="space-y-2">
+                <Label htmlFor="edit-customer_group" className="font-medium">
+                  Asiakasryhmä
+                </Label>
+                <Popover open={isCustomerGroupPopoverOpen} onOpenChange={setIsCustomerGroupPopoverOpen}>
+                  <PopoverTrigger asChild>
+                    <Button variant="outline" role="combobox" className={`w-full justify-between ${customer.customerGroup ? '' : 'placeholder'}`}>
+                      {customer.customerGroup || 'Valitse asiakasryhmä'}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-full p-0">
+                    <div className="p-2">
+                      <Input
+                        placeholder="Hae tai lisää"
+                        value={inputValueCustomerGroup}
+                        onChange={(e) => {
+                          setInputValueCustomerGroup(e.target.value)
                         }}
-                      >
-                        <Check className={cn('mr-2 h-4 w-4', customer.customerGroup === customerGroup ? 'opacity-100' : 'opacity-0')} />
-                        {customerGroup || 'Ei ryhmää'}
-                      </Button>
-                    ))}
-                  </div>
-                </PopoverContent>
-              </Popover>
+                        className="mb-2"
+                      />
+                      {inputValueCustomerGroup && !customerGroups.includes(inputValueCustomerGroup) && (
+                        <Button
+                          variant="ghost"
+                          className="w-full justify-start"
+                          onClick={handleCreateCustomerGroup}
+                        >
+                          <Plus className="w-4 h-4 mr-2" />
+                          Luo:
+                          {' '}
+                          {inputValueCustomerGroup}
+                        </Button>
+                      )}
+                    </div>
+                    <div className="max-h-[200px] overflow-y-auto">
+                      {customerGroups.sort((a, b) => a.localeCompare(b)).map(customerGroup => (
+                        <Button
+                          key={customerGroup}
+                          variant="ghost"
+                          className="w-full justify-start"
+                          onClick={() => {
+                            setCustomer({ ...customer, customerGroup })
+                            setIsCustomerGroupPopoverOpen(false)
+                          }}
+                        >
+                          <Check className={cn('mr-2 h-4 w-4', customer.customerGroup === customerGroup ? 'opacity-100' : 'opacity-0')} />
+                          {customerGroup || 'Ei ryhmää'}
+                        </Button>
+                      ))}
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
-          </div>
 
-          <h2 className="text-lg font-medium">Yhteystiedot</h2>
-          <div className="space-y-2">
-            <Label htmlFor="edit-address" className="font-medium">
-              Katuosoite
-            </Label>
-            <Input
-              id="edit-address"
-              value={customer.streetAddress || ''}
-              onChange={e => setCustomer({ ...customer, streetAddress: e.target.value })}
-              maxLength={255}
-            />
-          </div>
-
-          <div className="grid grid-cols-2 gap-4">
+            <h2 className="text-lg font-medium">Yhteystiedot</h2>
             <div className="space-y-2">
-              <Label htmlFor="edit-postal_code" className="font-medium">
-                Postinumero
+              <Label htmlFor="edit-address" className="font-medium">
+                Katuosoite
               </Label>
               <Input
-                id="edit-postal_code"
-                value={customer.postalCode || ''}
-                onChange={e => setCustomer({ ...customer, postalCode: e.target.value })}
-                maxLength={5}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="edit-city" className="font-medium">
-                Kaupunki
-              </Label>
-              <Input
-                id="edit-city"
-                value={customer.city || ''}
-                onChange={e => setCustomer({ ...customer, city: e.target.value })}
+                id="edit-address"
+                value={customer.streetAddress || ''}
+                onChange={e => setCustomer({ ...customer, streetAddress: e.target.value })}
                 maxLength={255}
               />
             </div>
-          </div>
 
-          <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="space-y-2">
+                <Label htmlFor="edit-postal_code" className="font-medium">
+                  Postinumero
+                </Label>
+                <Input
+                  id="edit-postal_code"
+                  value={customer.postalCode || ''}
+                  onChange={e => setCustomer({ ...customer, postalCode: e.target.value })}
+                  maxLength={5}
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="edit-city" className="font-medium">
+                  Kaupunki
+                </Label>
+                <Input
+                  id="edit-city"
+                  value={customer.city || ''}
+                  onChange={e => setCustomer({ ...customer, city: e.target.value })}
+                  maxLength={255}
+                />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <div className="space-y-2">
+                <Label htmlFor="edit-email" className="font-medium">
+                  Sähköposti
+                </Label>
+                <Input
+                  id="edit-email"
+                  type="email"
+                  value={customer.email || ''}
+                  onChange={e => setCustomer({ ...customer, email: e.target.value })}
+                  maxLength={255}
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="edit-phone" className="font-medium">
+                  Puhelin
+                </Label>
+                <Input
+                  id="edit-phone"
+                  value={customer.phone || ''}
+                  onChange={e => setCustomer({ ...customer, phone: e.target.value })}
+                  maxLength={255}
+                />
+              </div>
+            </div>
+            <h2 className="text-lg font-medium">Laskutuksen asetukset</h2>
             <div className="space-y-2">
-              <Label htmlFor="edit-email" className="font-medium">
-                Sähköposti
+              <Label htmlFor="edit-business_id" className="font-medium">
+                Y-tunnus
               </Label>
               <Input
-                id="edit-email"
-                type="email"
-                value={customer.email || ''}
-                onChange={e => setCustomer({ ...customer, email: e.target.value })}
+                id="edit-business_id"
+                placeholder="1234567-8"
+                value={customer.businessId || ''}
+                onChange={e => setCustomer({ ...customer, businessId: e.target.value })}
                 maxLength={255}
               />
             </div>
             <div className="space-y-2">
-              <Label htmlFor="edit-phone" className="font-medium">
-                Puhelin
-              </Label>
+              <span className="flex">
+                <Label htmlFor="edit-company_legal_name" className="font-medium">
+                  Yrityksen virallinen nimi
+                </Label>
+                {instructionTooltip('Yrityksen virallinen kaupparekisterissä oleva nimi, jota käytetään laskuissa. Jos tämä on tyhjä, käytetään nimeä.')}
+              </span>
               <Input
-                id="edit-phone"
-                value={customer.phone || ''}
-                onChange={e => setCustomer({ ...customer, phone: e.target.value })}
+                id="edit-company_legal_name"
+                placeholder="Oy Myymäläketju Ab"
+                value={customer.companyLegalName || ''}
+                onChange={e => setCustomer({ ...customer, companyLegalName: e.target.value })}
                 maxLength={255}
               />
             </div>
-          </div>
-          <h2 className="text-lg font-medium">Laskutuksen asetukset</h2>
-          <div className="space-y-2">
-            <Label htmlFor="edit-business_id" className="font-medium">
-              Y-tunnus
-            </Label>
-            <Input
-              id="edit-business_id"
-              placeholder="1234567-8"
-              value={customer.businessId || ''}
-              onChange={e => setCustomer({ ...customer, businessId: e.target.value })}
-              maxLength={255}
-            />
-          </div>
-          <div className="space-y-2">
-            <span className="flex">
-              <Label htmlFor="edit-company_legal_name" className="font-medium">
-                Yrityksen virallinen nimi
-              </Label>
-              {instructionTooltip('Yrityksen virallinen kaupparekisterissä oleva nimi, jota käytetään laskuissa. Jos tämä on tyhjä, käytetään nimeä.')}
-            </span>
-            <Input
-              id="edit-company_legal_name"
-              placeholder="Oy Myymäläketju Ab"
-              value={customer.companyLegalName || ''}
-              onChange={e => setCustomer({ ...customer, companyLegalName: e.target.value })}
-              maxLength={255}
-            />
-          </div>
-          <div className="space-y-2">
-            <div className="flex">
-              <Label htmlFor="edit-discount" className="font-medium">
-                Alennus (%)
-              </Label>
-              {instructionTooltip('Tämä asettaa yritykselle yleisen alennuksen, joka vaikuttaa kaikkiin laskuihin. Esimerkiksi 10,00 tarkoittaa 10 % alennusta.')}
+            <div className="space-y-2">
+              <div className="flex">
+                <Label htmlFor="edit-discount" className="font-medium">
+                  Alennus (%)
+                </Label>
+                {instructionTooltip('Tämä asettaa yritykselle yleisen alennuksen, joka vaikuttaa kaikkiin laskuihin. Esimerkiksi 10,00 tarkoittaa 10 % alennusta.')}
+              </div>
+              <Input
+                id="edit-discount"
+                placeholder="0,00"
+                value={customer.discount}
+                onChange={e =>
+                  setCustomer({
+                    ...customer,
+                    discount: e.target.value,
+                  })}
+                onBlur={(e) => {
+                  setCustomer({
+                    ...customer,
+                    discount: formatNumber(e.target.value),
+                  })
+                }}
+              />
             </div>
-            <Input
-              id="edit-discount"
-              placeholder="0,00"
-              value={customer.discount}
-              onChange={e =>
-                setCustomer({
-                  ...customer,
-                  discount: e.target.value,
-                })}
-              onBlur={(e) => {
-                setCustomer({
-                  ...customer,
-                  discount: formatNumber(e.target.value),
-                })
-              }}
-            />
-          </div>
-          <div className="space-y-2">
-            <span className="flex">
-              <Label htmlFor="edit-reference" className="font-medium">
-                Laskun viite
-              </Label>
-              {instructionTooltip('Voit määrittää viitekoodin, joka näkyy laskuissa.')}
-            </span>
-            <Input
-              id="edit-reference"
-              value={customer.invoiceReference || ''}
-              onChange={e => setCustomer({ ...customer, invoiceReference: e.target.value })}
-              maxLength={255}
-            />
-          </div>
-          <div className="flex items-center space-x-2 pt-2">
-            <span className="flex">
-              <Label htmlFor="edit-show_price_without_tax" className="font-medium">
-                Näytä hinnat ilman veroa
-              </Label>
-              {instructionTooltip('Jos olet sopinut asiakkaan kanssa ALV 0 % hinnat, valitse tämä. Tämän jälkeen laskuissa näytetään ALV 0 % hinnat. Tämä asetus on tehty, jotta vältytään pyöristysvirheiltä.')}
-            </span>
-            <Checkbox
-              id="edit-show_price_without_tax"
-              checked={customer.showPriceWithoutTax}
-              onCheckedChange={checked =>
-                setCustomer({ ...customer, showPriceWithoutTax: checked as boolean })}
-            />
+            <div className="space-y-2">
+              <span className="flex">
+                <Label htmlFor="edit-reference" className="font-medium">
+                  Laskun viite
+                </Label>
+                {instructionTooltip('Voit määrittää viitekoodin, joka näkyy laskuissa.')}
+              </span>
+              <Input
+                id="edit-reference"
+                value={customer.invoiceReference || ''}
+                onChange={e => setCustomer({ ...customer, invoiceReference: e.target.value })}
+                maxLength={255}
+              />
+            </div>
+            <div className="flex items-center space-x-2 pt-2">
+              <span className="flex">
+                <Label htmlFor="edit-show_price_without_tax" className="font-medium">
+                  Näytä hinnat ilman veroa
+                </Label>
+                {instructionTooltip('Jos olet sopinut asiakkaan kanssa ALV 0 % hinnat, valitse tämä. Tämän jälkeen laskuissa näytetään ALV 0 % hinnat. Tämä asetus on tehty, jotta vältytään pyöristysvirheiltä.')}
+              </span>
+              <Checkbox
+                id="edit-show_price_without_tax"
+                checked={customer.showPriceWithoutTax}
+                onCheckedChange={checked =>
+                  setCustomer({ ...customer, showPriceWithoutTax: checked as boolean })}
+              />
+            </div>
           </div>
         </div>
 
-        <DialogFooter>
-          <Button variant="outline" onClick={closeDialog}>
-            Peruuta
-          </Button>
+        <DialogFooter className="flex-shrink-0">
           <Button type="button" onClick={save}>
             <Save className="h-4 w-4 mr-2" />
             Tallenna muutokset
